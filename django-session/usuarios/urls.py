29cd9ce9e04452f9d6bdaca276011c392964eec2
LuisP30 <pimenta.lh2@gmail.com>
2024-03-28 10:41:02 -0300

    criação de páginas de login e cadastro

--- django-session/usuarios/urls.py
+++ django-session/usuarios/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('cadastro/', cadastro, name='cadastro'),
     path('valida_cadastro/', valida_cadastro, name='valida_cadastro'),
+    path('valida_login/', valida_login, name='valida_login'),
 ]
--- django-session/usuarios/views.py
+++ django-session/usuarios/views.py
@@ -5,10 +5,26 @@ from .models import *
 from hashlib import sha256
 
 def login(request):
-    return render(request, 'login.html')
+    status = request.GET.get('status') 
+    return render(request, 'login.html', context={'status': status})
 
+def valida_login(request):
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    senha = sha256(senha.encode()).hexdigest()
+    usuario = Usuario.objects.filter(email = email).filter(senha = senha)
+
+    if len(usuario) == 0:
+        return redirect(f'{reverse("usuarios:login")}?status=1')
+    elif len(usuario) > 0:
+        ...
+
+    return HttpResponse(f'{email} {senha}')
+
+# Views para cadastro abaixo:
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    status = request.GET.get('status')
+    return render(request, 'cadastro.html', context={'status': status})
 
 def valida_cadastro(request):
     nome = request.POST.get('nome')
@@ -16,13 +32,21 @@ def valida_cadastro(request):
     senha = request.POST.get('senha')
 
     if len(nome.strip())==0 or len(email.strip())==0:
-        return redirect(f"{reverse('usuarios:login')}?status=1")
-    if len(senha) > 8:
-        return redirect(f"{reverse('usuarios:login')}?status=2")
+        return redirect(f"{reverse('usuarios:cadastro')}?status=1")
+    if len(senha) < 8:
+        return redirect(f"{reverse('usuarios:cadastro')}?status=2")
     
-    senha = sha256(senha.encode()).hexdigest()
     usuario = Usuario.objects.filter(email = email)
     if len(usuario) > 0:
-        return redirect(f"{reverse('usuarios:login')}?status=3")
-
-    return HttpResponse(f'{nome}, {email}, {senha}')
+        return redirect(f"{reverse('usuarios:cadastro')}?status=3")
+    try:
+        senha = sha256(senha.encode()).hexdigest()
+        usuario = Usuario(
+            nome = nome,
+            email = email,
+            senha = senha
+        )
+        usuario.save()
+        return redirect(f'{reverse("usuarios:cadastro")}?status=0')
+    except:
+        return redirect(f'{reverse("usuarios:cadastro")}?status=4')
