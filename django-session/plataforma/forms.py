e949b60d90b5408aded7ca3f09a4e0d59660d4e1
LuisP30 <pimenta.lh2@gmail.com>
2024-04-06 23:09:07 -0300

    formularios do django

--- django-session/plataforma/forms.py
+++ django-session/plataforma/forms.py
@@ -3,3 +3,15 @@ from django import forms
 class Cliente(forms.Form):
     nome = forms.CharField(max_length=20)
     idade = forms.IntegerField()
+
+    def __init__(self, alterar = None, classe = None, *args, **kwargs): # Eu que criei as variáveis alterar e class
+        super().__init__(*args, **kwargs)
+        if alterar:
+            # Adicionando o atributo classe ao input nome
+            self.fields['nome'].widget.attrs['class'] = classe
+            # Adicionando o atributo placeholder ao input nome
+            self.fields['nome'].widget.attrs['placeholder'] = 'Digite seu nome'
+            # Adicionando o atributo classe ao input idade
+            self.fields['idade'].widget.attrs['class'] = classe
+            # Adicionando o atributo placeholder ao input idade
+            self.fields['idade'].widget.attrs['placeholder'] = 'Sua idade'
--- django-session/plataforma/views.py
+++ django-session/plataforma/views.py
@@ -7,7 +7,17 @@ from .forms import *
 
 @login_required(login_url = 'usuarios:login')
 def home(request):
-    form_cliente = Cliente()
+    form_cliente = Cliente(alterar=True, classe='teste')
+    input_alterar = ('nome', 'idade')
+    for i in form_cliente.fields.keys():
+        if i in input_alterar:
+            try:
+                classe_anterior = form_cliente.fields[i].widget.attrs['class']
+            except KeyError:
+                classe_anterior = ''
+            form_cliente.fields[i].widget.attrs['class'] = classe_anterior + ' form-control'
+            
+
     return render(request, 'home.html', context={
         'form': form_cliente
     })
