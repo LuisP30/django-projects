c5bf37b156199cb9113124f15f38f290d06d692a
LuisP30 <pimenta.lh2@gmail.com>
2024-05-04 09:45:25 -0300

    configurações celery

--- django-celery/celery_django/settings.py
+++ django-celery/celery_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'exemplo',
+    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_BROKER_URL = 'REDIS://127.0.0.1:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = 'django-db'
