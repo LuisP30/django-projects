b9c8089cae06ee6bf86994d28ecc09d93a15248c
LuisP30 <pimenta.lh2@gmail.com>
2024-07-29 15:33:22 -0300

    channels assincronos

--- django-channels/myapp/AsyncConsumers.py
+++ django-channels/myapp/AsyncConsumers.py
@@ -1,39 +1,38 @@
 # Importa o módulo 'json' para manipulação de dados em formato JSON
 import json  
 # Importa a classe 'WebsocketConsumer' da biblioteca Channels para criar consumidores WebSocket
-from channels.generic.websocket import WebsocketConsumer  
-from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer  
 
 # Define uma classe 'TesteConsumer' que herda de 'WebsocketConsumer'
-class TesteConsumer(WebsocketConsumer):  
+class TesteConsumer(AsyncWebsocketConsumer):  
     
     # Método chamado quando uma conexão WebSocket é estabelecida
-    def connect(self):
+    async def connect(self):
         # print(self.scope) # Scope é como se fosse a request 
         self.room_group_name = self.scope['url_route']['kwargs']['nome'] # Atribuindo um grupo ao usuário que conectou
         print(self.room_group_name)
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_add(
+                self.room_group_name,
+                self.channel_name
+            )
         
         # Aceita a conexão WebSocket. Isso permite que a comunicação WebSocket continue
-        self.accept()  
+        await self.accept()  
 
     # Método chamado quando a conexão WebSocket é fechada
-    def disconnect(self, close_code): 
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code): 
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Método chamado quando uma mensagem é recebida através da conexão WebSocket
-    def receive(self, text_data):  
+    async def receive(self, text_data):  
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -42,10 +41,10 @@ class TesteConsumer(WebsocketConsumer):
         )
 
     # Receive message from room group
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
--- django-channels/myapp/routing.py
+++ django-channels/myapp/routing.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from . import consumers
+from django.urls import re_path # Para que eu possa utilizar expressão regular
+from . import consumers, AsyncConsumers # Eu que inventei o nome AsyncConsumers para diferenciar do verdadeiro consumers
 
 websocket_url_patterns = [
-    path('ws/app/', consumers.TesteConsumer.as_asgi())
+    re_path(r'ws/app/(?P<nome>\w+)/$', AsyncConsumers.TesteConsumer.as_asgi()) # URL do Channels que recebe um parâmetro nome (regex)
 ]
