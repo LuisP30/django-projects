d6f6fe2002b7aa6e1156c8fedcdf6cc0d442939d
LuisP30 <pimenta.lh2@gmail.com>
2024-08-03 10:46:18 -0300

    introdução a testes de models

--- django-testes/usuarios/models.py
+++ django-testes/usuarios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField()
+    senha = models.CharField(max_length=50)
+    pontos = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.nome
+    
+    def add_pontos(self):
+        self.pontos += 10
+    
--- django-testes/usuarios/tests/tests_models.py
+++ django-testes/usuarios/tests/tests_models.py
@@ -1,7 +1,21 @@
 from django.test import TestCase
+from ..models import Usuario
 
 class UsuarioTesteCase(TestCase):
     
+    def setUp(self):
+        Usuario.objects.create(
+            nome = 'Luis',
+            email = 'luis@email.com',
+            senha = '1234'
+        )
     
-    def test_um_e_um(self):
-        self.assertEqual(1, 1)
+    def test_model_criado_no_banco(self):
+        usuario = Usuario.objects.get(nome = 'Luis')
+        
+        self.assertEqual(usuario.__str__(), 'Luis')
+        
+    def test_add_pontos(self):
+        usuario = Usuario.objects.get(nome = 'Luis')
+        usuario.add_pontos()
+        self.assertEqual(usuario.pontos, 10)
