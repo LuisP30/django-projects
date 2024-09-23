c64f8e3eca348dd17b9c4b7c63e0693c29127648
LuisP30 <pimenta.lh2@gmail.com>
2024-09-23 00:26:22 -0300

    pratica de cache

--- escalabilidade/teste/views.py
+++ escalabilidade/teste/views.py
@@ -1,2 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from .models import Usuario
 
+@cache_page(60) # 1 minuto
+def teste(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'teste.html', context={
+        'usuarios': usuarios,
+    })
